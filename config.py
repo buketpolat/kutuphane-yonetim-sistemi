e204ca1168562721dcec19579ae37848f3825daf
Nehir Buket Polat <buketpolat08@outlook.com>
2025-11-04 17:57:27 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:buket1234@localhost:5432/akilli_kutuphane"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/akilli_kutuphane"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "supersecretkey"
